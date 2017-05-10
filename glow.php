5d7a5543bb3119ac46fd69e916c85d9555d41102
GoldFash <goldfashhosting@users.noreply.github.com>
2017-05-10 09:58:08 -0400

    Update glow.php

--- glow.php
+++ glow.php
@@ -2,22 +2,27 @@
 
 /**
  * @package GLOW
- * @version 2.0
+ * @version 2.0.1
  */
 /*
 Plugin Name: GLOW
 Plugin URL: http://goldfash.com?plugins
 Description: GLOW provides various functions for GMWP+. GLOW is also the heartbeat of using GMWP+.
-Version: 2.0
+Version: 2.0.1
 Author: GOD1ST.Cloud Developers
 Author URI:        http://GOD1st.Cloud
 Contributors:      raceanf
 Domain Path:       /languages
-Text Domain:       glow
-GitHub Plugin URI: https://github.com/goldfashhosting/glow
+Text Domain:       GLOW
+GitHub Plugin URI: https://github.com/goldfashhosting/GLOW
 GitHub Branch:     master
 */
 
 
-require_once ('inc/gfunctions/functions.php');
-include ('inc/webapps/shortcodes.php');
+function glow_init()
+{
+ require ('inc/gfunctions/functions.php');
+ require ('inc/webapps/shortcodes.php');
+}
+add_action('init', 'glow_init');
+

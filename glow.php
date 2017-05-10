25c7e8663c44ac9ac745e6e983746462079ba437
GoldFash <goldfashhosting@users.noreply.github.com>
2017-05-10 08:24:25 -0400

    Creation of Glow.php

---
+++ glow.php
@@ -0,0 +1,11 @@
+<?php
+/*
+  Plugin Name: GLOW
+  Version: 1.0
+  Plugin URI: http://goldfash.com
+  Author: GOD1ST.Cloud Developers
+  Author URI: http://GOD1st.Cloud
+  Description: GLOW provides various functions for GMWP+. GLOW is also the heartbeat of using GMWP+.
+ */
+
+
